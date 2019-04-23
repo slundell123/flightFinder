f5d2da43e7fc4158ea60ae70fd0678f66fd0b3d5
slundell123 <s_lundell@att.net>
2019-04-22 21:07:59 -0600

    Fixed some user information issues
    would ask the user if they would like to enter info, but would not actually ask for any info. Added some cin.ignore() statements to fix.

--- driver.cpp
+++ driver.cpp
@@ -19,6 +19,7 @@ string runFile(){
     cout << "Enter CSV file name" << endl;
     string file;
     cin >> file;
+    cin.ignore();
     return file;
   } else {
     char filename[100];
@@ -43,6 +44,7 @@ string runFile(){
       else returningFile += filename[i];
     }
     returningFile += "-flight-results.csv";
+    //cin.ignore();
     return returningFile;
   }
 }
@@ -78,145 +80,144 @@ int main(int argc, char* argv[]){
   string theFile;
 
   
-    theFile = runFile();
-    ifstream myfile;
-    string file = theFile;
-    myfile.open(file);
-    if(myfile.is_open()){
-      while(getline(myfile, stops, ',')){
-        if(stops != "stops"){
-          if(stops == "Nonstop"){
-            FI.stops = 0;
-          }
-          else if(stops == "1 Stop"){
-            FI.stops = 1;
-          }
-          else if(stops == "2 Stop"){
-            FI.stops = 2;
-          }
-          else if(stops == "3 Stop"){
-            FI.stops = 3;
-          }
+  theFile = runFile();
+  ifstream myfile;
+  string file = theFile;
+  myfile.open(file);
+  if(myfile.is_open()){
+    while(getline(myfile, stops, ',')){
+      if(stops != "stops"){
+        if(stops == "Nonstop"){
+          FI.stops = 0;
         }
-        getline(myfile, priceS, ',');
-          if(priceS != "ticket price"){
-            FI.price = stof(priceS);
-          }
-        getline(myfile, departure, ',');
-          if(departure != "departure"){
-            FI.departure = departure;
-          }
-        getline(myfile, arrival, ',');
-          if(arrival != "arrival"){
-            FI.arrival = arrival;
-          }
-        getline(myfile, duration, ',');
-          if(duration != "flight duration"){
-            FI.duration = duration;
-          }
-        getline(myfile, airline, ',');
-          if(airline != "airline"){
-            FI.airline = airline;
-          }
-        getline(myfile, plane, ',');
-          if(plane != "plane"){
-            FI.plane = plane;
-          }
-        getline(myfile, timings, ',');
-          if(timings != "timings"){
-            FI.timings = timings;
-          }
-        getline(myfile, flightCode);
-        if(flightCode != "plane code"){
-          FI.flightCode = flightCode;
+        else if(stops == "1 Stop"){
+          FI.stops = 1;
         }
-        v.push_back(FI);
+        else if(stops == "2 Stop"){
+          FI.stops = 2;
+        }
+        else if(stops == "3 Stop"){
+          FI.stops = 3;
+        }
+      }
+      getline(myfile, priceS, ',');
+        if(priceS != "ticket price"){
+          FI.price = stof(priceS);
+        }
+      getline(myfile, departure, ',');
+        if(departure != "departure"){
+          FI.departure = departure;
+        }
+      getline(myfile, arrival, ',');
+        if(arrival != "arrival"){
+          FI.arrival = arrival;
+        }
+      getline(myfile, duration, ',');
+        if(duration != "flight duration"){
+          FI.duration = duration;
+        }
+      getline(myfile, airline, ',');
+        if(airline != "airline"){
+          FI.airline = airline;
+        }
+      getline(myfile, plane, ',');
+        if(plane != "plane"){
+          FI.plane = plane;
+        }
+      getline(myfile, timings, ',');
+        if(timings != "timings"){
+          FI.timings = timings;
+        }
+      getline(myfile, flightCode);
+      if(flightCode != "plane code"){
+        FI.flightCode = flightCode;
+      }
+      v.push_back(FI);
+    }
+
+  cout << "Would you like to input user information? (Y/N)" << endl;
+  getline(cin, userInfoYN);
+  if(userInfoYN == "Y" || userInfoYN == "y"){
+    cout << "Please enter your name: " << endl;
+    getline(cin, userName);
+    cout << "Welcome, " << userName << "!" << endl;
+    cout << "What is your home airport?" << endl;
+    getline(cin, homeAirport);
+  }
+  else if(userInfoYN == "N" || userInfoYN == "Y"){
+    cout << "Okay, maybe next time" << endl;
+  }
+  while(userChoice != "4"){
+    menu();
+    getline(cin, userChoice);
+    switch(stoi(userChoice)){
+      case 1:{
+        BST Price;
+        for(int i = 1; i < v.size(); i++){
+          Price.addNodePRICE(v[i].stops, v[i].price, v[i].departure, v[i].arrival, v[i].duration, v[i].airline, v[i].plane, v[i].timings, v[i].flightCode);
+        }
+        cout << "*****************************" << endl << endl;
+        cout << "This is the flight we chose for you " << userName << endl;
+        Price.printLeft();
+        cout << "Would you like to see all other flight options? (Y/N)" << endl;
+        getline(cin, allPrice);
+        if(allPrice == "Y" || allPrice == "y"){
+          cout << "Here are all of the available flights: " << endl;
+          Price.printTree();
+        }
+        else{
+          cout << "Okay!" << endl;
+        }
+        cout << "*****************************" << endl << endl;
+        break;
+      }
+      case 2:{
+        BST Stops;
+        for(int i = 1; i < v.size(); i++){
+          Stops.addNodeSTOPS(v[i].stops, v[i].price, v[i].departure, v[i].arrival, v[i].duration, v[i].airline, v[i].plane, v[i].timings, v[i].flightCode);
+        }
+        cout << "*****************************" << endl;
+        cout << "This is the flight we chose for you " << userName << endl;
+        Stops.printLeft();
+        cout << "Would you like to see all of your options? (Y/N)" << endl;
+        getline(cin, allStop);
+        if(allStop == "Y" || allStop == "y"){
+          cout << "Here are all of the available flights: " << endl;
+          Stops.printTree();
+        }
+        else{
+          cout << "Okay!" << endl;
+        }
+        cout << "*****************************" << endl;
+        break;
+      }
+      case 3:{
+        BST Dur;
+        for(int i = 1; i < v.size(); i++){
+          Dur.addNodeDUR(v[i].stops, v[i].price, v[i].departure, v[i].arrival, v[i].duration, v[i].airline, v[i].plane, v[i].timings, v[i].flightCode);
+        }
+        cout << "*****************************" << endl;
+        cout << "This is the flight that we chose for you " << userName << endl;
+        Dur.printLeft();
+        cout << "Would you like to see all of your options? (Y/N)" << endl;
+        getline(cin, allDur);
+        if(allDur == "Y" || allDur == "y"){
+          cout << "Here are all of the available flights: " << endl;
+          Dur.printTree();
+        }
+        else{
+          cout << "Okay!" << endl;
+        }
+        cout << "*****************************" << endl;
+        break;
       }
 
-    cout << "Would you like to input user information? (Y/N)" << endl;
-    cin.ignore();
-    getline(cin, userInfoYN);
-    if(userInfoYN == "Y" || userInfoYN == "y"){
-      cout << "Please enter your name: " << endl;
-      getline(cin, userName);
-      cout << "Welcome, " << userName << "!" << endl;
-      cout << "What is your home airport?" << endl;
-      getline(cin, homeAirport);
-    }
-    else if(userInfoYN == "N" || userInfoYN == "Y"){
-      cout << "Okay, maybe next time" << endl;
-    }
-    while(userChoice != "4"){
-      menu();
-      getline(cin, userChoice);
-      switch(stoi(userChoice)){
-        case 1:{
-          BST Price;
-          for(int i = 1; i < v.size(); i++){
-            Price.addNodePRICE(v[i].stops, v[i].price, v[i].departure, v[i].arrival, v[i].duration, v[i].airline, v[i].plane, v[i].timings, v[i].flightCode);
-          }
-          cout << "*****************************" << endl << endl;
-          cout << "This is the flight we chose for you " << userName << endl;
-          Price.printLeft();
-          cout << "Would you like to see all other flight options? (Y/N)" << endl;
-          getline(cin, allPrice);
-          if(allPrice == "Y" || allPrice == "y"){
-            cout << "Here are all of the available flights: " << endl;
-            Price.printTree();
-          }
-          else{
-            cout << "Okay!" << endl;
-          }
-          cout << "*****************************" << endl << endl;
-          break;
-        }
-        case 2:{
-          BST Stops;
-          for(int i = 1; i < v.size(); i++){
-            Stops.addNodeSTOPS(v[i].stops, v[i].price, v[i].departure, v[i].arrival, v[i].duration, v[i].airline, v[i].plane, v[i].timings, v[i].flightCode);
-          }
-          cout << "*****************************" << endl;
-          cout << "This is the flight we chose for you " << userName << endl;
-          Stops.printLeft();
-          cout << "Would you like to see all of your options? (Y/N)" << endl;
-          getline(cin, allStop);
-          if(allStop == "Y" || allStop == "y"){
-            cout << "Here are all of the available flights: " << endl;
-            Stops.printTree();
-          }
-          else{
-            cout << "Okay!" << endl;
-          }
-          cout << "*****************************" << endl;
-          break;
-        }
-        case 3:{
-          BST Dur;
-          for(int i = 1; i < v.size(); i++){
-            Dur.addNodeDUR(v[i].stops, v[i].price, v[i].departure, v[i].arrival, v[i].duration, v[i].airline, v[i].plane, v[i].timings, v[i].flightCode);
-          }
-          cout << "*****************************" << endl;
-          cout << "This is the flight that we chose for you " << userName << endl;
-          Dur.printLeft();
-          cout << "Would you like to see all of your options? (Y/N)" << endl;
-          getline(cin, allDur);
-          if(allDur == "Y" || allDur == "y"){
-            cout << "Here are all of the available flights: " << endl;
-            Dur.printTree();
-          }
-          else{
-            cout << "Okay!" << endl;
-          }
-          cout << "*****************************" << endl;
-          break;
-        }
-
-        case 4:
-          cout << "Thank you!" << endl;
-          break;
-        default:
-          cout << "Invalid Choice" << endl;
-        }
+      case 4:
+        cout << "Thank you!" << endl;
+        break;
+      default:
+        cout << "Invalid Choice" << endl;
       }
     }
   }
+}
